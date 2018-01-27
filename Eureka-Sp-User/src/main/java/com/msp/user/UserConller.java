56ff4e743d2c09f4286728b536e49c07a3cd5785
qiuguochang <qiuguochang@localhost>
2018-01-26 22:20:05 -0800

    3

--- Eureka-Sp-User/src/main/java/com/msp/user/UserConller.java
+++ Eureka-Sp-User/src/main/java/com/msp/user/UserConller.java
@@ -2,16 +2,21 @@ package com.msp.user;
 
 import javax.websocket.server.PathParam;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserConller {
+	@Autowired
+	UserDao dao;
+	
 	@GetMapping("/{id}")
-	String idex(@PathVariable Long id)
+	public EntityUser findById(@PathVariable Long id)
 	{
-		return 
+		EntityUser user=this.dao.findOne(id);
+		return user;
 	}
 
 }
