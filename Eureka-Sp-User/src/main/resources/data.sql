7a4ebdc7cc4541afae696c1b50817535e2dbd0ae
qiuguochang <qiuguochang@localhost>
2018-01-27 00:16:31 -0800

    3

---
+++ Eureka-Sp-User/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into user (id ,username,name,age,blance) values (1,'ac1','dd1', 32, 120.32);
+insert into user (id ,username,name,age,blance) values (2,'ac2','df2',12,133.32);
+insert into user (id ,username,name,age,blance) values (3,'ac3','fd4',4,14.32);
+insert into user (id ,username,name,age,blance) values (4,'ac4','bb5',56,14.32);
+insert into user (id ,username,name,age,blance) values (5,'ac5','jj6',23,126.32);
+insert into user (id ,username,name,age,blance) values (6,'ac6','mn7',19,128.32);
