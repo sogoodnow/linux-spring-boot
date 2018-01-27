d9b1a070d932fd9e1219eceb2c341fdc3b0e20f7
qiuguochang <qiuguochang@localhost>
2018-01-26 20:44:14 -0800

    new

---
+++ Eureka-Sp-User/src/main/java/com/msp/user/EurekaSpUserApplication.java
@@ -0,0 +1,12 @@
+package com.msp.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaSpUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaSpUserApplication.class, args);
+	}
+}
